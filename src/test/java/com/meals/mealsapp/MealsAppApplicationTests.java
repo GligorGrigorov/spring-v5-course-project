331b113fd904f91bda19b4ec6461fd7fb10dfaa7
Gligor Grigorov <grigorovgligor@gmail.com>
2022-01-31 20:05:46 +0200

    Add initial maven project

---
+++ src/test/java/com/meals/mealsapp/MealsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meals.mealsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
